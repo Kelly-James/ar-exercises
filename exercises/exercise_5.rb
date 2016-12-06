bf6ba435f326317ef7331b30d227f38500cabf2d
Kelly James <kellyjamesclancy@gmail.com>
2016-12-06 22:32:25 +0000

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,17 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum_revenue = Store.sum("annual_revenue")
+
+pp @sum_revenue
+
+@store_count = Store.count
+
+pp @store_count
+
+pp @sum_revenue / @store_count
+
+@high_revenue = Store.where("annual_revenue > ?", 1000000).count
+
+pp @high_revenue
